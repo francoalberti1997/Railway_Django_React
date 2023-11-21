bae7ae37ee0bb73df21ee8260ed344ffd61f65c5
FrancoAlberti1997 <albertifranco1997@hotmail.com>
2023-11-21 11:07:41 -0300

    4th commit

--- ProjectPrueba/views.py
+++ ProjectPrueba/views.py
@@ -2,6 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from prueba_api import models
 def index(request):
-
-
-    return HttpResponse("Hola Mundo  sss")
+    return HttpResponse("Hola Mundo Actualizado")
