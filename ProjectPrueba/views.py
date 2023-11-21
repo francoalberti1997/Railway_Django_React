44c2ea7f5d3332e3972add51fa2c79aefb0a6ed9
FrancoAlberti1997 <albertifranco1997@hotmail.com>
2023-11-21 11:19:11 -0300

    conectando db

--- ProjectPrueba/views.py
+++ ProjectPrueba/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from prueba_api import models
+
 def index(request):
-    return HttpResponse("Hola Mundo Actualizado")
+    persona = models.Persona.objects.all()
+    
+    return HttpResponse(persona)
--- prueba_api/models.py
+++ prueba_api/models.py
@@ -6,3 +6,7 @@ class Persona(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     edad = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.nombre
+    
