045bd217c3eef52943b298fb82e48a7a5be60ef5
FrancoAlberti1997 <albertifranco1997@hotmail.com>
2023-11-21 13:28:21 -0300

    4s

--- ProjectPrueba/views.py
+++ ProjectPrueba/views.py
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-from django.shortcuts import render
-from django.http import HttpResponse
-from prueba_api import models
-
-def index(request):
-    persona = models.Persona.objects.all()
-    return render("index.html")
-=======
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, "index.html")
->>>>>>> master
